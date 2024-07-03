3d32e94636a41a5e716b9fe436dfa650b8e5aeb5
Leighton Solomon <leightonsolo@gmail.com>
2024-07-03 16:12:56 -0500

    Update TimeoutWarning.js

--- TimeoutWarning.js
+++ TimeoutWarning.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         WIP Warning on Unconfirmed Calibration
 // @namespace    https://github.com/LeightonSolo/IsensixScripts
-// @version      0.6
+// @version      0.7
 // @description  Will warn if you have sat on the calibration confirmation page for too long without confirming before the page times out
 // @author       Leighton Solomon
 // @match        https://*/arms2/calibration/calsensor.php
 // @match        https://*/arms/calsensor.php
 // @match        https://*/arms2/calsensor.php
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=isensix.com
+// @downloadURL  https://raw.githubusercontent.com/LeightonSolo/IsensixScripts/main/TimeoutWarning.js
+// @updateURL    https://raw.githubusercontent.com/LeightonSolo/IsensixScripts/main/TimeoutWarning.js
 // @grant        window.focus
 // ==/UserScript==
 
