e6c5fe2a75711d9fea3adc5735df894ee7b33465
Leighton Solomon <leightonsolo@gmail.com>
2025-10-28 12:41:14 -0400

    add support for g3.0
    Updated the version number and adjusted logic for detecting Guardian server versions. Enhanced button creation for sensor calibration.

--- CalibrateButtonsOnLiveView.js
+++ CalibrateButtonsOnLiveView.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Calibrate Buttons for Live View, ARMS and Guardian
 // @namespace    https://github.com/LeightonSolo/IsensixScripts
-// @version      4.01
+// @version      5
 // @description  Adds a Calibration button to every sensor on the Isensix Live view page, will work on all ARMS and Guardian servers, Exception buttons WIP
 // @author       Leighton Solomon
 // @match        https://*/arms2/index.php*
 // @match        https://*/arms2/
 // @match        https://*/arms/
 // @match        https://*/arms/index.php*
+// @match        https://*/guardian/index.php*
 // @downloadURL  https://raw.githubusercontent.com/LeightonSolo/IsensixScripts/main/CalibrateButtonsOnLiveView.js
 // @updateURL    https://raw.githubusercontent.com/LeightonSolo/IsensixScripts/main/CalibrateButtonsOnLiveView.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=isensix.com
@@ -36,6 +37,15 @@ let twoPointZero = 0;
     }
     catch(err){}
 
+let threePoint0 = false;
+    try {//determine if the server is Guardian 3.0
+        if(document.querySelector("#isemainmenu > li:nth-child(1) > a").title == "Guardian 3.0"){
+            threePoint0 = true;
+            console.log("3.0 Detected");
+        }
+    }
+    catch(err){}
+
 const url = document.URL;
 let calUrl = "";
 if(arms == 1){
@@ -46,6 +56,9 @@ else if(url.slice(33, 38) == "arms/"){
 }
 else if(twoPointZero == 1){ //sets calibration URL for Guardian 2.0
     calUrl = url.slice(0, 39) + "calsensor.php?id=";
+}
+else if(threePoint0 == 1){ //sets calibration URL for Guardian 3.0
+    calUrl = url.slice(0, 42) + "calibration/calsensor.php?id=";
 }
 else{ //sets calibration URL for Guardian 2.1
     calUrl = url.slice(0, 39) + "calibration/calsensor.php?id=";
@@ -60,37 +73,72 @@ function openCalUrl(id, butt){
     'use strict';
 
     if(arms == 0){//if system is detected to be guardian
+        if(threePoint0){ //G3.0
+            let sensorList = document.getElementsByClassName("slnk"); //gets list of all sensors on the live view page
 
-        let sensorList = document.getElementsByClassName("slnk poplink"); //gets list of all sensors on the live view page
+            const sensorList2 = document.getElementsByClassName("lvhead");
 
-        if (sensorList.length === 0){
-            sensorList = document.getElementsByClassName("slnk "); //handle weird 2.0 servers that have a different class for sensor name
-        }
-        const sensorList2 = document.getElementsByClassName("mon");
+            for (let i = 0; i < sensorList.length; i++) {
+                let id = sensorList[i].onclick.toString().slice(33, 37); //gets the ID of the sensor from the html
+                let butt=document.createElement("button");
 
-        for (let i = 0; i < sensorList.length; i++) {
-            let id = sensorList[i].onclick.toString().slice(33, 37); //gets the ID of the sensor from the html
-            let butt=document.createElement("button");
+                id = id.match(/(\d+)/)[0]; //gets only the digits from the sensor ID if it contains trailing ");"
 
-            id = id.match(/(\d+)/)[0]; //gets only the digits from the sensor ID if it contains trailing ");"
+                butt.innerHTML="Cal";
 
-            butt.innerHTML="Calibrate";
+                butt.addEventListener("click", () => openCalUrl(id, butt));
 
-            butt.addEventListener("click", () => openCalUrl(id, butt));
+                //sensorList2[i].appendChild(butt);
 
-            //sensorList2[i].appendChild(butt);
+                let cell = document.createElement("td");
+                // Get first row of table
+                /*let row = sensorList2[i].rows[0];
+                // Create new row if row doesn't exist
+                if(!row) {
+                    row = document.createElement("row");
+                    sensorList2[i].appendChild(row);
+                }*/
+                                    sensorList2[i].appendChild(butt);
 
-            let cell = document.createElement("td");
-            // Get first row of table
-            let row = sensorList2[i].rows[0];
-            // Create new row if row doesn't exist
-            if(!row) {
-                row = document.createElement("row");
-                sensorList2[i].appendChild(row);
+                // Add button to sensor table cell
+                //cell.appendChild(butt);
+                //row.prepend(cell);
             }
-            // Add button to sensor table cell
-            cell.appendChild(butt);
-            row.prepend(cell);
+
+        }
+        else{ //G2.1 and G2.0
+
+            let sensorList = document.getElementsByClassName("slnk poplink"); //gets list of all sensors on the live view page
+
+            if (sensorList.length === 0){
+                sensorList = document.getElementsByClassName("slnk "); //handle weird 2.0 servers that have a different class for sensor name
+            }
+            const sensorList2 = document.getElementsByClassName("mon");
+
+            for (let i = 0; i < sensorList.length; i++) {
+                let id = sensorList[i].onclick.toString().slice(33, 37); //gets the ID of the sensor from the html
+                let butt=document.createElement("button");
+
+                id = id.match(/(\d+)/)[0]; //gets only the digits from the sensor ID if it contains trailing ");"
+
+                butt.innerHTML="Calibrate";
+
+                butt.addEventListener("click", () => openCalUrl(id, butt));
+
+                //sensorList2[i].appendChild(butt);
+
+                let cell = document.createElement("td");
+                // Get first row of table
+                let row = sensorList2[i].rows[0];
+                // Create new row if row doesn't exist
+                if(!row) {
+                    row = document.createElement("row");
+                    sensorList2[i].appendChild(row);
+                }
+                // Add button to sensor table cell
+                cell.appendChild(butt);
+                row.prepend(cell);
+            }
         }
     }
     else if(arms == 1){ //if system is detected to be ARMS
