d25814cb7ad86d0b382708c5fffcc46a9eae3eb8
Leighton Solomon <leightonsolo@gmail.com>
2026-03-10 12:35:28 -0500

    Update script name and version for Live View

--- CalibrateButtonsOnLiveView.js
+++ CalibrateButtonsOnLiveView.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Calibrate Buttons for Live View, ARMS and Guardian
+// @name         Calibrate Buttons for Live View (ARMS, G2.0, G2.1, G3.0)
 // @namespace    https://github.com/LeightonSolo/IsensixScripts
-// @version      5.39
+// @version      5.40
 // @description  Adds a Calibration button to every sensor on the Isensix Live view page, will work on all ARMS and Guardian servers, Exception buttons WIP
 // @author       Leighton Solomon
 // @match        https://*/arms2/index.php*
