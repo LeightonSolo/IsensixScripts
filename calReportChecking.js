320bb2f356f7e8c4c4630d4f080bd519d7aa18b5
Leighton Solomon <leightonsolo@gmail.com>
2026-04-14 15:01:59 -0500

    fix samples text handling
    3/3 is "3/
                          3" for some reason on certain Guardian versions??? added a fix for that

--- calReportChecking.js
+++ calReportChecking.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Calibration Report Error Checking (ARMS, G2.0, G2.1, G3.0)
 // @namespace    https://github.com/LeightonSolo/IsensixScripts
-// @version      1.3
+// @version      1.4
 // @description  Ensures canned messages correct, meter matches sensor type, and correct amount of samples entered - ARMS, Guardian 2.0, 2.1, and 3.0
 // @author       Leighton Solomon
 // @match        https://*.isensix.com/cust/?id=*
@@ -65,7 +65,8 @@
         }
 
         if (samplesCell) {
-            const samplesText = samplesCell.textContent.trim();
+            let samplesText = samplesCell.textContent.trim();
+            samplesText = samplesText.replace(/\s/g, ''); //remove newline which is in 3/3 for some reason??
             if (!validSamples.includes(samplesText)) markRed(samplesCell);
         }
 
